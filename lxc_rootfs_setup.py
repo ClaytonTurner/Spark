625cfa41bfdadd4cffda44ee596f68af702f529d
Clayton Turner <learn2mine@gmail.com>
2015-03-31 03:09:28 -0400

    Removing print

--- lxc_rootfs_setup.py
+++ lxc_rootfs_setup.py
@@ -8,7 +8,6 @@ loc = "/var/lib/lxc"
 for spark_vm in os.listdir(loc):
 	if spark_vm.split("6")[0] == "spark":
 		print loc+"/"+spark_vm+"/fstab"
-		fstab_str = "/mnt/gv1/spark      /var/lib/lxc/"+spark_vm+"/rootfs/data        none    bind,create=dir"
 		f = open(loc+"/"+spark_vm+"/fstab","w")
 		f.write(fstab_str)
 		f.close()
