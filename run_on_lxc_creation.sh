875703bab5f54fcb9be8dfed31bb6d0fcd2899dc
Clayton Turner <learn2mine@gmail.com>
2015-04-01 04:19:09 -0400

    Adding spark download to lxc execution script

--- run_on_lxc_creation.sh
+++ run_on_lxc_creation.sh
@@ -8,11 +8,17 @@ apt-get --assume-yes install python-numpy python-scipy python-matplotlib ipython
 apt-get --assume-yes install gfortran
 wget http://www.netlib.org/blas/blas.tgz
 gunzip blas.tgz
-tar xf blas.tar
+tar -xf blas.tar
+rm blas.tgz
+rm blas.tar
 cd BLAS
 gfortran -c *.f
 ar rv libblas.a *.o
 su -c "cp libblas.a /usr/local/lib"
 apt-get --assume-yes install python-pip
 pip install --upgrade --no-deps git+https://github.com/Theano/Theano.git
- 
+wget http://www.gtlib.gatech.edu/pub/apache/spark/spark-1.3.0/spark-1.3.0.tgz
+gunzip spark-1.3.0.tgz
+tar -xf spark-1.3.0.tar 
+rm spark-1.3.0.tgz
+rm spark-1.3.0.tar  
