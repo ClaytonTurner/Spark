2cfaeaf871aa953aefe0a5728c27403c6af85080
Clayton Turner <learn2mine@gmail.com>
2015-04-06 03:21:17 -0400

    Updating sgd script

--- spark_sgd.py
+++ spark_sgd.py
@@ -1,5 +1,25 @@
 from pyspark import SparkContext
 
-dataFile = "/data/spark/Spark/iris_labelFirst.data" #this is located on spark6a (the master node)
-sc = SparkContext(appName="Spark SGD")
-data = sc.textFile(logFile).cache()
+parameters = None 
+accrued_gradients = 0
+
+def get_accrued_gradients():
+	return accrued_gradients
+def set_accrued_gradients(grad):
+	global accrued_gradients # Needed to modify accrued_gradients globally
+	accrued_gradients += grad
+
+def startAsyncFetchParams(parameters):
+
+if __name__ == "__main__":
+	dataFile = "/data/spark/Spark/iris_labelFirst.data" 	
+	sc = SparkContext(appName="Spark SGD")
+	data = sc.textFile(logFile).cache()
+		
+	step = 0
+	accrued_gradients = 0
+	while(True):
+		if step%n_fetch == 0:
+			startAsyncFetchParams(parameters)
+
+	sc.stop()
