bfba0770f3d9053ef1aac6b6702fa4c7c4f0f6d6
ClaytonTurner <caturner3@g.cofc.edu>
2015-05-15 12:13:28 -0400

    Adding client python file

---
+++ client.py
@@ -0,0 +1,13 @@
+import sys
+from socket import socket, AF_INET, SOCK_DGRAM
+
+IP_SERVER = "192.168.137.51"
+PORT = 5000
+SIZE = 1024
+print "Test client sending packets to IP {0}, via port {1}\n".format(IP_SERVER, PORT)
+
+my_socket = socket(AF_INET, SOCK_DGRAM)
+
+while True:
+	my_socket.sendto('cool',(IP_SERVER,PORT))
+sys.exit()
