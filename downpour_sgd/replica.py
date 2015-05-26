f62c6e1ca178b090bff9945fad262a8b874e37be
ClaytonTurner <caturner3@g.cofc.edu>
2015-05-26 15:39:04 -0400

    Setting up downpour - using static IP VM's now

---
+++ downpour_sgd/replica.py
@@ -0,0 +1,15 @@
+import sys
+from socket import socket, AF_INET, SOCK_DGRAM
+
+IP_SERVER = "192.168.137.50"
+#IP_SERVER = "192.168.137.154"
+#PORT = 5000
+PORT = 45001
+SIZE = 1024
+print "Test client sending packets to IP {0}, via port {1}\n".format(IP_SERVER, PORT)
+
+my_socket = socket(AF_INET, SOCK_DGRAM)
+
+while True:
+	my_socket.sendto('cool',(IP_SERVER,PORT))
+sys.exit()
