a7af1953d2d234a209ce10d76a8d6e1c78c680e5
ClaytonTurner <caturner3@g.cofc.edu>
2015-05-26 15:29:28 -0400

    Update everything

--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -1,9 +1,9 @@
-if [ $# -eq 1 ] i
+if [ $# -eq 1 ] 
 	then
 		con_type=$1
 		docker rm $(docker ps -aq)
 		sudo docker build -t ${con_type}_container .
-		sudo docker run --name ${con_type}_instance -m 256m -d -p 45001:11211 server_container
+		sudo docker run --name ${con_type}_instance -m 256m -d -p 45001:11211 ${con_type}_container
 		docker logs ${con_type}_instance
 	else
 		echo "Incorrect usage. sh rebuild.sh <name_type>"
