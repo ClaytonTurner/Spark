5e34031990c274af00a3624441f653033a65ca25
ClaytonTurner <caturner3@g.cofc.edu>
2015-05-27 12:43:29 -0400

    Adding communication helper file

---
+++ downpour_sgd/my_communication.py
@@ -0,0 +1,23 @@
+
+from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
+import sys
+
+
+def send_data(data, loc):
+	# Send data to location provided in loc (IP)
+	PORT = loc
+	SIZE = 1024 # May need to alter
+	hostname = gethostbyname('0.0.0.0')
+
+	my_socket = socket(AF_INET, SOCK_DGRAM)
+	my_socket.bind( (hostname, PORT) )
+
+	print "Test server listening on port {0}\n".format(PORT)
+	return True # Signifies no error
+
+def receive_data():
+	data = None
+	while data is not None:
+		# Should probably add a timeout
+		(data,addr) = my_socket.recvfrom(SIZE)
+	return data # Signifies no error
