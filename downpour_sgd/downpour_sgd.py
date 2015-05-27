2673d2740283dc2d94a344d7ed517bd7480510ea
ClaytonTurner <caturner3@g.cofc.edu>
2015-05-27 15:04:28 -0400

    Moving code to right places

--- downpour_sgd/downpour_sgd.py
+++ downpour_sgd/downpour_sgd.py
@@ -1,5 +1,4 @@
 import sys
-from pyspark import SparkContext
 from sklearn import linear_model as lm # for gradient descent; adapted from https://gist.github.com/MLnick/4707012
 from sklearn.base import copy
 import random
@@ -69,41 +68,4 @@ def avg_model(sgd, slices): # Part of aforementioned adaptation
 	sgd.coef_ /= slices
 	sgd.intercept_ /= slices
 	return sgd
-
-if __name__ == "__main__":
-	if len(sys.argv) != 5:
-		print >> sys.stderr, "Usage: spark_sgd.py <data_file> <alpha> <n_fetch> <n_push>"
-		exit(-1)
-
-	slices = 10 # Arbitrary - Empirically tune for performance
-
-	#data_file = "/data/spark/Spark/iris_labelFirst.data" 	
-	data_file = str(sys.argv[1])
-	alpha = float(sys.argv[2])
-	n_fetch = 1 #int(sys.argv[3]) # Google fixed to 1 in paper
-	n_push = 1 #int(sys.argv[4]) # Google fixed to 1 in paper
-
-	step = 0
-	accrued_gradients = get_accrued_gradients()
-
-	#TODO NN initialization
-
-	while(True):
-		if step == 0:
-			parameters = init_parameters()
-		if step > 1000: # This can be tweaked
-			break 
-		if step%n_fetch == 0: # Always true in fixed case
-			startAsynchronouslyFetchingParameters(parameters)
-		data = getNextMinibatch(data)
-		#gradient = sc.parallelize(data, numSlices=slices) \
-		#	.mapPartitions(lambda x: computeGradient(parameters,x) \
-		#	.reduce(lambda x, y: merge(x,y))
-		gradient = avg_model(gradient, slices)
-		set_accrued_gradients(gradient)
-		parameters -= alpha*gradient #TODO as parameters is currently a dictionary
-		if step%n_push == 0: # Always true in fixed case
-			startAsynchronouslyPushingGradients(accrued_gradients)
-		step += 1
-
 
