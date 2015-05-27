2126a75dc3765ea9a4de8c73fff9b2e899ef33ca
ClaytonTurner <caturner3@g.cofc.edu>
2015-05-27 14:37:35 -0400

    Removing init from downpour

--- downpour_sgd/downpour_sgd.py
+++ downpour_sgd/downpour_sgd.py
@@ -16,14 +16,6 @@ SERVER_IP = "192.168.137.50"
 PARAM_PORT = 45001
 GRAD_PORT = 45002
 
-def init_parameters(data_len): # Probably moving to distbelief at some point
-	hidden_layers = [100,100,100]
-	weight_dist = 1./sqrt(data_len)
-	weights = [[random.uniform(-weight_dist,weight_dist) for y in hidden_layers] for x in range(len(hidden_layers)+1)]
-	# so we have 4 sets of weights since we need to set up the output layer
-	# Now we push the initialized updates to the param server 
-	return {"hidden_layers":hidden_layers,"weights":weights}
-
 def get_accrued_gradients():
 	data = comm.receive_data(GRAD_PORT)
 	return data
