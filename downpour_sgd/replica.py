92b9ecd05dc48e478a2d111b8c69e19143d1db37
ClaytonTurner <caturner3@g.cofc.edu>
2015-05-27 15:06:10 -0400

    Moving code to right files

--- downpour_sgd/replica.py
+++ downpour_sgd/replica.py
@@ -1,4 +1,9 @@
+import downpour_sgd as d_sgd
 
+
+learning_rate = 0.1 # tune later on
+n_fetch = 1 # fixed in the paper, so let's leave it that way here
+n_push = 1 # same as n_fetch
 
 def init_parameters(data_len):
         hidden_layers = [100,100,100]
@@ -7,4 +12,39 @@ def init_parameters(data_len):
         # so we have 4 sets of weights since we need to set up the output layer
         # Now we push the initialized updates to the param server
         return {"hidden_layers":hidden_layers,"weights":weights}
+
+def init_accrued_gradients():
+	#TODO
+	return 0.
+
+if __name__ == "__main__":
+        slices = 10 # Arbitrary - Empirically tune for performance
+
+        #data_file = "/data/spark/Spark/iris_labelFirst.data"
+        data_file = str(sys.argv[1])
+
+        step = 0
+        accrued_gradients = init_accrued_gradients()
+
+        #TODO NN initialization
+
+        while(True):
+                if step == 0:
+                        parameters = init_parameters()
+                if step > 1000: # This can be tweaked
+                        break
+                if step%n_fetch == 0: # Always true in fixed case
+                        startAsynchronouslyFetchingParameters(parameters)
+                data = getNextMinibatch(data)
+                #gradient = sc.parallelize(data, numSlices=slices) \
+                #       .mapPartitions(lambda x: computeGradient(parameters,x) \
+                #       .reduce(lambda x, y: merge(x,y))
+                gradient = avg_model(gradient, slices)
+                set_accrued_gradients(gradient)
+                parameters -= alpha*gradient #TODO as parameters is currently a dictionary
+                if step%n_push == 0: # Always true in fixed case
+                        startAsynchronouslyPushingGradients(accrued_gradients)
+                step += 1
+
+
 
