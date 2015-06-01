38550145648cf83a0c91511289c16531140ba3e4
ClaytonTurner <caturner3@g.cofc.edu>
2015-06-01 16:06:30 -0400

    Updating paramserver

--- downpour_sgd/param_server.py
+++ downpour_sgd/param_server.py
@@ -3,48 +3,39 @@ This file is for the param server which will host gradient descent updates
  to be read by and written to by the distbelief model replicas
 '''
 
-#from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
-import socket
 import sys
-import threading
-import SocketServer
-from time import sleep
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+from math import sqrt
+import random
 
 HOST = socket.gethostname()
-PORT = 45001
+PORT = 8000
 SIZE = 4096 # Find out how big our messages will be then fix this
 	    # Or send a size first and handle that then
 	    # TODO
 
+learning_rate = .2
+params = list() 
 
+def initialize():
+	global params
+	data_len = 10 # UPDATE THIS
+	hidden_layers = [100,100,100]
+        weight_dist = 1./sqrt(data_len)
+        weights = [[random.uniform(-weight_dist,weight_dist) for y in hidden_layers] for x in range(len(hidden_layers)+1)]
+	params = weights
+		
 
-'''
-hostname = gethostbyname('0.0.0.0')
-
-my_socket = socket(AF_INET, SOCK_DGRAM)
-my_socket.bind( (hostname, PORT) )
-
-print "Test server listening on port {0}\n".format(PORT)
-
-weights = []
-
-while True:
-	(data,addr) = my_socket.recvfrom(SIZE)
-	# So we have received an update and it is stored in the variable data
-	
-	print data
-sys.exit()
-'''
-
-def test_func(*nums):
-	return sum(nums)
+def test_func(nums):
+	return nums
 
 if __name__ == "__main__":
-	# Port 0 means to select an arbitrary unused port
-	#HOST, PORT = "localhost", 0 # defined earlier in file
-	server = SimpleXMLRPCServer(("localhost",8000))
-	print "Listening on port 8000..."
-	server.register_function(test_func,"test function");
-	server.serve_forever()
+	self.initialize()
+	server = SimpleXMLRPCServer(("localhost",PORT))
+	print "Listening on port "+str(PORT)+"..."
+	server.register_function(test_func);
+	try:
+		server.serve_forever()
+	except KeyboardInterrupt:
+		print "Keyboard interrupt: exiting"
 
--- downpour_sgd/replica.py
+++ downpour_sgd/replica.py
@@ -9,18 +9,6 @@ learning_rate = 0.1 # tune later on
 n_fetch = 1 # fixed in the paper, so let's leave it that way here
 n_push = 1 # same as n_fetch
 
-def init_parameters(data_len=5): # tweek that later. just trying to pass interpretation error-less
-        hidden_layers = [100,100,100]
-        weight_dist = 1./sqrt(data_len)
-        weights = [[random.uniform(-weight_dist,weight_dist) for y in hidden_layers] for x in range(len(hidden_layers)+1)]
-        # so we have 4 sets of weights since we need to set up the output layer
-        # Now we push the initialized updates to the param server
-        return {"hidden_layers":hidden_layers,"weights":weights}
-
-def init_accrued_gradients():
-	#TODO
-	return 0.
-
 if __name__ == "__main__":
         slices = 10 # Arbitrary - Empirically tune for performance
 
@@ -45,8 +33,7 @@ if __name__ == "__main__":
                         parameters = init_parameters()
 
 		proxy = xmlrpclib.ServerProxy("http://localhost:8000/")
-		print("Output should be 6\n")
-		print(proxy.test_func([1,2,3])	
+		print(proxy.test_func([1,2,3]))	
 
                 #if step%n_fetch == 0: # Always true in fixed case
                 #        startAsynchronouslyFetchingParameters(parameters)
@@ -60,7 +47,7 @@ if __name__ == "__main__":
                 #if step%n_push == 0: # Always true in fixed case
                 #        startAsynchronouslyPushingGradients(accrued_gradients)
                 step += 1
-	sock.close()
+	#sock.close()
 
 
 
