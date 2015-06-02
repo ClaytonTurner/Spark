e9e7af1e67ef137dd8caef86db1814e2587e9fde
ClaytonTurner <caturner3@g.cofc.edu>
2015-06-02 11:56:33 -0400

    xmlrpc setup working

--- downpour_sgd/param_server.py
+++ downpour_sgd/param_server.py
@@ -7,6 +7,7 @@ import sys
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from math import sqrt
 import random
+import socket
 
 HOST = socket.gethostname()
 PORT = 8000
@@ -14,7 +15,6 @@ SIZE = 4096 # Find out how big our messages will be then fix this
 	    # Or send a size first and handle that then
 	    # TODO
 
-learning_rate = .2
 params = list() 
 
 def initialize():
@@ -26,14 +26,26 @@ def initialize():
 	params = weights
 		
 
-def test_func(nums):
-	return nums
+def getNextMinibatch():
+	#TODO 
+	return '' 
+
+def startAsynchronouslyPushingGradients(grad):
+	# Update the gradients on the server
+	return True
+
+def startAsynchronouslyFetchingParameters():
+	global params
+	return params
 
 if __name__ == "__main__":
-	self.initialize()
+	print "Starting Param Server. Ctrl + C to quit\n"
+	initialize()
 	server = SimpleXMLRPCServer(("localhost",PORT))
 	print "Listening on port "+str(PORT)+"..."
-	server.register_function(test_func);
+	server.register_function(getNextMinibatch)
+	server.register_function(startAsynchronouslyPushingGradients)
+	server.register_function(startAsynchronouslyFetchingParameters)
 	try:
 		server.serve_forever()
 	except KeyboardInterrupt:
--- downpour_sgd/replica.py
+++ downpour_sgd/replica.py
@@ -1,4 +1,3 @@
-import downpour_sgd as d_sgd
 import socket
 import sys
 from math import sqrt
@@ -9,46 +8,45 @@ learning_rate = 0.1 # tune later on
 n_fetch = 1 # fixed in the paper, so let's leave it that way here
 n_push = 1 # same as n_fetch
 
+'''
+Parameter and Gradient initialization will now happen on the param server
+we can do an initial read from all replicas if needbe, but there is no
+	reason to re-calculate that information for each replica
+	if it will be the same from the start
+'''
+
+def compute_gradient(params,alpha):
+	return None
+
 if __name__ == "__main__":
         slices = 10 # Arbitrary - Empirically tune for performance
 
         #data_file = "/data/spark/Spark/iris_labelFirst.data"
-        #data_file = str(sys.argv[1])
 
         step = 0
-	###sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP socket
-
-        accrued_gradients = init_accrued_gradients()
 	
 	HOST = socket.gethostname()
-	PORT, SIZE = 45001, 1024
+	PORT, SIZE = 8000, 1024
         
+	proxy = xmlrpclib.ServerProxy("http://localhost:"+str(PORT)+"/",allow_none=True)
+
 	#TODO NN initialization
-
+	grad_push_errors = 0
         while(step < 5):
-		## Move to different file later
-		# Need here later, but not ready to test
-                
-		if step == 0:
-                        parameters = init_parameters()
-
-		proxy = xmlrpclib.ServerProxy("http://localhost:8000/")
-		print(proxy.test_func([1,2,3]))	
-
-                #if step%n_fetch == 0: # Always true in fixed case
-                #        startAsynchronouslyFetchingParameters(parameters)
-                #data = d_sgd.getNextMinibatch(data)
-                ##gradient = sc.parallelize(data, numSlices=slices) \
-                ##       .mapPartitions(lambda x: computeGradient(parameters,x) \
-                ##       .reduce(lambda x, y: merge(x,y))
-                #gradient = d_sgd.avg_model(gradient, slices)
-                #d_sgd.set_accrued_gradients(gradient)
+                if step%n_fetch == 0: # Always true in fixed case
+                        parameters = proxy.startAsynchronouslyFetchingParameters()
+                data = proxy.getNextMinibatch()
+		accrued_gradients = compute_gradient(parameters,learning_rate)
                 #parameters -= alpha*gradient #TODO as parameters is currently a dictionary
-                #if step%n_push == 0: # Always true in fixed case
-                #        startAsynchronouslyPushingGradients(accrued_gradients)
+                if step%n_push == 0: # Always true in fixed case
+			boolean = proxy.startAsynchronouslyPushingGradients(accrued_gradients)
+			print boolean
+			if boolean is not True:
+				grad_push_errors += 1
                 step += 1
-	#sock.close()
+	print "Gradient Pushing Errors: "+str(grad_push_errors)+"\n"
 
-
-
-
+ ##gradient = sc.parallelize(data, numSlices=slices) \
+ ##       .mapPartitions(lambda x: computeGradient(parameters,x) \
+ ##       .reduce(lambda x, y: merge(x,y))
+                
