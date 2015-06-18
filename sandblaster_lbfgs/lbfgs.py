2766e7a6ae11dc30ad2d9242e8013efd02d6b13e
cassios <cassios.kmm@gmail.com>
2015-06-18 15:57:43 -0400

    fix bug on lbfgs and set up NN to train iris data set

--- sandblaster_lbfgs/lbfgs.py
+++ sandblaster_lbfgs/lbfgs.py
@@ -1,7 +1,14 @@
 import numpy as np
 from scipy.optimize.linesearch import line_search_wolfe1
+from scipy.optimize.linesearch import line_search_wolfe2
 
-def fmin_LBFGS(func, x0, funcprime, maxIter=1000):
+def f(x):
+	return .5*(1 - x[0])**2 + (x[1] - x[0]**2)**2		
+def fprime(x):		
+	return np.array((-2*.5*(1 - x[0]) - 4*x[0]*(x[1] - x[0]**2), 2*(x[1] - x[0]**2)))
+
+
+def fmin_LBFGS(func, x0, funcprime, args=(), maxIter=1000):
 	"""
 	func: callable f(x)
 		Objective function to be minimized.
@@ -11,26 +18,29 @@ def fmin_LBFGS(func, x0, funcprime, maxIter=1000):
 		Gradient of f.
 	maxIter: int, optional
 		Maximum number of Iterations to perform
+
+	return:
+		x_opt: ndarray
 	"""
-	x0 = np.asarray(x0).flatten()
+	x0 = np.asarray(x0, dtype=np.float64).flatten()
 	if(x0.ndim == 0):
 		x0.shape = (1,)
 
 	len_x = x0.size
 	x_k = x0
-	old_fval = func(x0)
+	old_fval = func(x0, *args)
 	old_old_fval = None
-	gf_k = funcprime(x0)
-	invHessian_B = np.identity(len_x)
+	gf_k = funcprime(x0, *args)
+	invHessian_B = np.identity(len_x, dtype=np.float64)
 
 	maxHistory = 10
-	history_S = np.empty(shape=(maxHistory, len_x))
-	history_Y = np.empty(shape=(maxHistory, len_x))
-	rho = np.empty(maxHistory)
+	history_S = np.empty(shape=(maxHistory, len_x), dtype=np.float64)
+	history_Y = np.empty(shape=(maxHistory, len_x), dtype=np.float64)
+	rho = np.empty(maxHistory, dtype=np.float64)
 	warnFlag = 0
 	step_K = 0
 
-	gtol = 1e-5
+	gtol = 1e-50
 	gnorm = np.linalg.norm(gf_k, np.inf)
 
 	while (gnorm > gtol) and (step_K < maxIter):
@@ -39,8 +49,10 @@ def fmin_LBFGS(func, x0, funcprime, maxIter=1000):
 		d_k = computeDirection(maxHistory, step_K, gf_k, invHessian_B, history_S, history_Y, rho)
 
 		#lineSearch 
+		# alpha_K, fc, gc, old_fval, old_old_fval, gf_kp1 = \
+		# 					line_search_wolfe1(func, funcprime, x_k, d_k, gf_k, old_fval, old_old_fval, args=args)
 		alpha_K, fc, gc, old_fval, old_old_fval, gf_kp1 = \
-							line_search_wolfe1(func, funcprime, x_k, d_k, gf_k, old_fval, old_old_fval)
+							line_search_wolfe2(func, funcprime, x_k, d_k, gf_k, args=args)
 		if(alpha_K is None):
 			# Line search failed to find a better solution.
 			warnFlag = 1
@@ -51,8 +63,7 @@ def fmin_LBFGS(func, x0, funcprime, maxIter=1000):
 		#define the index to update the arrays S and Y
 		#the history arrays, S and Y, are reused for eficiency
 		indexHistory = step_K % maxHistory
-		indexHistory = indexHistory if(indexHistory < (maxHistory - 1)) else 0
-
+		
 		#save new pair
 		history_S[indexHistory] = s_k = np.subtract(x_kp1, x_k)
 		history_Y[indexHistory] = y_k = gf_kp1 - gf_k
@@ -68,25 +79,23 @@ def fmin_LBFGS(func, x0, funcprime, maxIter=1000):
 
 		if(not np.isfinite(old_fval)):
 			#optimal value is +-Inf
-			warnFlag = 1
+			warnFlag = 2
 			break
 
 		step_K += 1
 	
 	if(warnFlag == 1):
+		print "Stopped because line search did not converge"
+	if(warnFlag == 2):
 		print "Stopped due to precission loss"
 
-	x_opt = x_k
-	fval = old_fval
-	print("***Current function value: %f" % fval)
-	print("***Iterations: %d" % step_K)
-	return x_opt
+	return x_k
 
 
 def computeDirection(maxHistory, step_K, gf_k, B_k, history_S, history_Y, rho):
 	
 	upperBound = maxHistory if(step_K > maxHistory) else step_K
-	alpha = np.empty(upperBound)
+	alpha = np.empty(upperBound, dtype=np.float64)
 
 	for i in range(upperBound-1, -1, -1):
 		alpha[i] = rho[i] * np.dot(history_S[i], gf_k)
@@ -99,24 +108,6 @@ def computeDirection(maxHistory, step_K, gf_k, B_k, history_S, history_Y, rho):
 		r =  np.add(r, history_S[i] * (alpha[i] - beta))
 
 	return -r
-
-
-def f(x):   # The rosenbrock function
-	return .5*(1 - x[0])**2 + (x[1] - x[0]**2)**2
-def fprime(x):
-	return np.array((-2*.5*(1 - x[0]) - 4*x[0]*(x[1] - x[0]**2), 2*(x[1] - x[0]**2)))
-
-def BoothFunc(x):
-	return (x[0] + 2*x[1] - 7)**2 + (2*x[0] + x[1] - 5)**2
-
-def BoothFprime(x):
-	return np.array((10*x[0] + 8*x[1] - 34, 10*x[1] + 8*x[0] - 38))
-
-def MatyasFunc(x):
-	return 0.26*(x[0]**2 + x[1]**2) - 0.48*x[0]*x[1]
-
-def MatyasFprime(x):
-	return np.array((0.52*x[0] - 0.48*x[1], 0.52*x[1] + 0.48*x[0]))
 
 if __name__ == '__main__':
 	print fmin_LBFGS(f, [2.,2.], fprime)
