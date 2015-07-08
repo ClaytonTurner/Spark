507009128cc2963ceab42aeb63f25df38001124a
cassios <cassios.kmm@gmail.com>
2015-07-08 13:30:18 -0400

    include biases node in downpour/neural_net

--- downpour_sgd/neural_net.py
+++ downpour_sgd/neural_net.py
@@ -1,106 +1,103 @@
 import numpy as np
 
 def sigmoid(x):
-    return 1.0/(1.0 + np.exp(-x))
+	return 1.0/(1.0 + np.exp(-x))
 
 def sigmoid_prime(x):
-    return sigmoid(x)*(1.0-sigmoid(x))
+	return sigmoid(x)*(1.0-sigmoid(x))
 
 def tanh(x):
-    return np.tanh(x)
+	return np.tanh(x)
 
 def tanh_prime(x):
-    return 1.0 - x**2
+	return 1.0 - x**2
 
 
 class NeuralNetwork:
 
-    def __init__(self, layers, activation='tanh'):
-        if activation == 'sigmoid':
-            self.activation = sigmoid
-            self.activation_prime = sigmoid_prime
-        elif activation == 'tanh':
-            self.activation = tanh
-            self.activation_prime = tanh_prime
+	def __init__(self, layers, activation='tanh'):
+		if activation == 'sigmoid':
+			self.activation = sigmoid
+			self.activation_prime = sigmoid_prime
+		elif activation == 'tanh':
+			self.activation = tanh
+			self.activation_prime = tanh_prime
 
-        # Set weights
-        self.weights = []
-        # layers = [2,2,1]
-        # range of weight values (-1,1)
-        # input and hidden layers - random((2+1, 2+1)) : 3 x 3
-        for i in range(1, len(layers) - 1):
-            r = 2*np.random.random((layers[i-1] + 1, layers[i] + 1)) -1
-            self.weights.append(r)
-        # output layer - random((2+1, 1)) : 3 x 1
-        r = 2*np.random.random( (layers[i] + 1, layers[i+1])) - 1
-        self.weights.append(r)
+		#Wij is the weight from node i in layer l-1 to node j in layer l
+		#for a NN with layers = [2,2,2] :
+			# input and hidden layers - (2+1, 2) : 3 x 2
+			# output layer - (2+1, 2) : 3 x 2
+		self.weights = []
+		for i in range(1, len(layers) - 1):
+			r = 2*np.random.random((layers[i-1] + 1, layers[i])) -1
+			self.weights.append(r)
+		# output layer - random((2+1, 1)) : 3 x 1
+		r = 2*np.random.random( (layers[i] + 1, layers[i+1])) - 1
+		self.weights.append(r)
 
-    def set_weights(self,params):
-	self.weights = params
+	def set_weights(self,params):
+		self.weights = params
 
-    def fit(self, X, y, learning_rate=0.2, epochs=100000):
-        # Add column of ones to X
-        # This is to add the bias unit to the input layer
-        ones = np.atleast_2d(np.ones(X.shape[0]))
-        X = np.concatenate((ones.T, X), axis=1)
-        for k in range(epochs):
-            if k % 10000 == 0: print 'epochs:', k
-            #if k % 10000 == 0: print 'weights:',self.weights 
-            i = np.random.randint(X.shape[0])
-            a = [X[i]]
+	def feed_forward(self, x):
+		a = [x]
+		for l in range(len(self.weights)):
+			inputs = np.hstack((1, a[l])) #add the biases node
+			activation = self.activation(np.dot(inputs, self.weights[l]))
+			a.append(activation)
+		return a
 
-            for l in range(len(self.weights)):
-		    #print "a[l]:",a[l]
-		    #print "self.weights[l]:",self.weights[l]
-                    dot_value = np.dot(a[l], self.weights[l])
-                    activation = self.activation(dot_value)
-                    a.append(activation)
-            # output layer
-            error = y[i] - a[-1]
-            deltas = [error * self.activation_prime(a[-1])]
+	def fit(self, X, y, learning_rate=0.2, epochs=50000):
+		for k in range(epochs):
+			if k % 10000 == 0: print 'epochs:', k
+			#if k % 10000 == 0: print 'weights:',self.weights 
+			i = np.random.randint(X.shape[0])
+			
+			a = self.feed_forward(X[i])
+			
+			# output layer
+			error = y[i] - a[-1]
+			deltas = [error * self.activation_prime(a[-1])]
 
-            # we need to begin at the second to last layer 
-            # (a layer before the output layer)
-            for l in range(len(a) - 2, 0, -1): 
-                deltas.append(deltas[-1].dot(self.weights[l].T)*self.activation_prime(a[l]))
+			# we need to begin at the second to last layer 
+			# (a layer before the output layer)
+			for l in range(len(self.weights) - 1, 0, -1): 
+				weights_tmp = self.weights[l][1:] #remove biases node's weights
+				deltas.append(deltas[-1].dot(weights_tmp.T)*self.activation_prime(a[l]))
 
-            # reverse
-            # [level3(output)->level2(hidden)]  => [level2(hidden)->level3(output)]
-            deltas.reverse()
+			# reverse
+			# [level3(output)->level2(hidden)]  => [level2(hidden)->level3(output)]
+			deltas.reverse()
 
-            # backpropagation
-            # 1. Multiply its output delta and input activation 
-            #    to get the gradient of the weight.
-            # 2. Subtract a ratio (percentage) of the gradient from the weight.
-            for i in range(len(self.weights)):
-                layer = np.atleast_2d(a[i])
-                delta = np.atleast_2d(deltas[i])
-		self.weights[i] = np.array(self.weights[i]).copy() # This prevents discontiguous memory errors
-               	self.weights[i] += learning_rate * layer.T.dot(delta)
-		print "test",layer.T.dot(delta)
+			# backpropagation
+			# 1. Multiply its output delta and input activation 
+			#    to get the gradient of the weight.
+			# 2. Subtract a ratio (percentage) of the gradient from the weight.
+			grad = np.array(())
+			for i in range(len(self.weights)):
+				a_tmp = np.hstack((1, a[i]))
+				delta_l =  np.outer(deltas[i], a_tmp).T
+				self.weights[i] += learning_rate * delta_l
 
 
-    def predict(self, x): 
-        a = np.concatenate((np.ones(1).T, np.array(x)), axis=1)     
-        for l in range(0, len(self.weights)):
-            a = self.activation(np.dot(a, self.weights[l]))
-        return a
+	def predict(self, x): 
+		a = self.feed_forward(x)
+		return a[-1]
 
 if __name__ == '__main__':
 
-    print "This is a test of the neural net on the XOR problem\n"
+	print "This is a test of the neural net on the XOR problem\n"
 
-    #nn = NeuralNetwork([2,2,1])
-    nn = NeuralNetwork([2,2,2])
-    X = np.array([[0, 0],
-                  [0, 1],
-                  [1, 0],
-                  [1, 1]])
+	#nn = NeuralNetwork([2,2,1])
+	nn = NeuralNetwork([2,2,2])
+	X = np.array([[0, 0],
+				  [0, 1],
+				  [1, 0],
+				  [1, 1]])
 
-    #y = np.array([0, 1, 1, 0])
-    y = np.array([[1,0],[0,1],[0,1],[1,0]])
+	#y = np.array([0, 1, 1, 0])
+	y = np.array([[1,0],[0,1],[0,1],[1,0]])
 
-    nn.fit(X, y)
+	nn.fit(X, y)
 
-    for e in X:
-        print(e,nn.predict(e))
+	for e in X:
+		print(e,nn.predict(e))
